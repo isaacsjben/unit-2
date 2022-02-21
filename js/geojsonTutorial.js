bffd6ec4d2e1c94707a159a1de3b7ea6a9d462b3
isaacsjben <bisaacs2@wisc.edu>
2022-02-21 14:01:26 -0600

    update 2/21

---
+++ js/geojsonTutorial.js
@@ -0,0 +1,17 @@
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJSON(myLines, {
+    style: myStyle
+}).addTo(map);
--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-// Add all scripts to the JS folder
+var map;
+
+function createMap(){
+    map = L.map('map').setView([53, 5], 4);
+//L.tileLayer: Instantiates a tile layer object given a URL template and optionally an options object.
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 4,
+	maxZoom: 6,
+	ext: 'png'
+//.addTo: Adds the layer to the given map or layer group.
+}).addTo(map);
+
+getData(map)
+};
+
+//function to retrieve the data and place it on the map
+function getData(map){
+    //load the data
+    fetch("data/Europe-ParliamentData.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){            
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                },
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        });
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
