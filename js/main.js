e76d0de8d29b0f6df86edac9bb5be0cf270e96f4
isaacsjben <bisaacs2@wisc.edu>
2022-03-01 15:34:32 -0600

    update 3/1

--- js/main.js
+++ js/main.js
@@ -15,6 +15,18 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}
 getData(map)
 };
 
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
 //function to retrieve the data and place it on the map
 function getData(map){
     //load the data
